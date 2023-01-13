d389a44370ec8b33ac0fc7b0ec55b1f97ebcf104
casey <dev@null.com>
2023-01-12 19:08:46 -0800

    Make the actual expansion code in romexpander.py its own function.

--- mystic/romexpand.py
+++ mystic/romexpand.py
@@ -16,8 +16,8 @@ def romExpand():
 #  romExpandIpsPatch('./roms/colorization/en_uk_kkzero.ips')
 
 #################################################
-def romExpandMoveMaps():
-  """" This patches the rom before encoding, to add extra capabilities. """
+def romExpand32Banks():
+  """ This patches the rom before encoding, to add extra space. """
 
   # changing the rom header, see
   # https://gbdev.gg8.se/wiki/articles/The_Cartridge_Header
@@ -33,10 +33,15 @@ def romExpandMoveMaps():
   bank0[0x014d] = 0xb8
 
   # we add 16 more banks
-  for i in range(0,16):
+  for i in range(len(mystic.romSplitter.banks), 32):
     clean = [0x00] * 0x4000
     mystic.romSplitter.banks.append(clean)
 
+#################################################
+def romExpandMoveMaps():
+  """ This patches the rom before encoding, to clear the area where maps used to live. Moving maps is handled by the addr file. """
+
+  romExpand32Banks()
 
   bank5 = mystic.romSplitter.banks[5]
   # we clean bank5, where map 00 (the overworld) used to be. 
@@ -44,29 +49,11 @@ def romExpandMoveMaps():
   for i in range(0,0x4000):
     bank5[i] = 0xff
 
-
 #################################################
 def romExpandMoveMusicBank():
   """" Moving the music bank 0xF around """
 
-  # changing the rom header, see
-  # https://gbdev.gg8.se/wiki/articles/The_Cartridge_Header
-  bank0 = mystic.romSplitter.banks[0]
-
-  # Cartridge Type = MBC5+RAM+BATTERY
-  bank0[0x0147] = 0x1b
-  # ROM size = 32 banks
-  bank0[0x0148] = 0x04
-  # RAM Size = 02
-  bank0[0x0149] = 0x02
-  # Header Checksum
-  bank0[0x014d] = 0xb8
-
-  # we add 16 more banks
-  for i in range(0,16):
-    clean = [0x00] * 0x4000
-    mystic.romSplitter.banks.append(clean)
-
+  romExpand32Banks()
 
   bankf = mystic.romSplitter.banks[0xf]
   bank17 = mystic.romSplitter.banks[0x17]
