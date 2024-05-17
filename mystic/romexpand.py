80d49b0817f6b0cd9ddc9d5e4afff2c3f8a7132a
xenophile127 <dev@null.com>
2024-05-17 16:33:03 -0700

    Enable moving the music bank and expanding scripts to three or four banks.

--- mystic/romexpand.py
+++ mystic/romexpand.py
@@ -6,18 +6,20 @@ def romExpand():
 
   print('rom expanding...')
 
+#  romExpandMBC5()
+
   # choose one (or none) of the following available rom expansions,
   # you can also make your own custom romExpand...()
 
-  romExpandMoveMaps()
+#  romExpandMoveMaps()
 #  romExpandMoveMusicBank()
-#  romExpandMoveMusicBankAndExpandScriptsToFourBanks()
+  romExpandMoveMusicBankAndExpandScriptsToFourBanks()
 #  romExpandIpsPatch('./roms/colorization/en_uk_256.ips')
 #  romExpandIpsPatch('./roms/colorization/en_uk_kkzero.ips')
 
 #################################################
-def romExpandMoveMaps():
-  """" This patches the rom before encoding, to add extra capabilities. """
+def romExpandMBC5():
+  """" This patches the rom to MBC5 and expands it to 32 banks. """
 
   # changing the rom header, see
   # https://gbdev.gg8.se/wiki/articles/The_Cartridge_Header
@@ -37,6 +39,9 @@ def romExpandMoveMaps():
     clean = [0x00] * 0x4000
     mystic.romSplitter.banks.append(clean)
 
+#################################################
+def romExpandMoveMaps():
+  """" This patches the rom before encoding, to add extra capabilities. """
 
   bank5 = mystic.romSplitter.banks[5]
   # we clean bank5, where map 00 (the overworld) used to be. 
@@ -49,49 +54,30 @@ def romExpandMoveMaps():
 def romExpandMoveMusicBank():
   """" Moving the music bank 0xF around """
 
-  # changing the rom header, see
-  # https://gbdev.gg8.se/wiki/articles/The_Cartridge_Header
-  bank0 = mystic.romSplitter.banks[0]
+  newbanknr = 0x1f
 
-  # Cartridge Type = MBC5+RAM+BATTERY
-  bank0[0x0147] = 0x1b
-  # ROM size = 32 banks
-  bank0[0x0148] = 0x04
-  # RAM Size = 02
-  bank0[0x0149] = 0x02
-  # Header Checksum
-  bank0[0x014d] = 0xb8
-
-  # we add 16 more banks
-  for i in range(0,16):
-    clean = [0x00] * 0x4000
-    mystic.romSplitter.banks.append(clean)
-
-
-  bankf = mystic.romSplitter.banks[0xf]
-  bank17 = mystic.romSplitter.banks[0x17]
-  for i in range(0,0x4000):
-    # muevo el banco 0xf a 0x17
-    bank17[i] = bankf[i]
-    # y borro el banco 0xf
-    bankf[i] = 0xff
-
-  
+  # get the existing music bank from the assembly
   bank0 = mystic.romSplitter.banks[0x0]
+  oldbanknr = bank0[0x2053]
+
+  oldbank = mystic.romSplitter.banks[oldbanknr]
+  newbank = mystic.romSplitter.banks[newbanknr]
+  for i in range(0,0x4000):
+    # muevo el banco old a new
+    newbank[i] = oldbank[i]
+    if oldbanknr != newbanknr:
+      # y borro
+      oldbank[i] = 0xff
+
   # set the music bank in asm
-  bank0[0x2053] = 0x17
-  bank0[0x217c] = 0x17
+  bank0[0x2053] = 0x1f
+  bank0[0x217c] = 0x1f
 
 #################################################
 def romExpandMoveMusicBankAndExpandScriptsToFourBanks():
 
   # first we move the music bank
   romExpandMoveMusicBank()
-
-  bankf = mystic.romSplitter.banks[0x0f]
-  # we clean bankf
-  for i in range(0,0x4000):
-    bankf[i] = 0xff
 
   # hacked asm for supporting more than 2 script banks (thanks @xenophile!)  
   improved = [0xfa, 0xb6, 0xd8, 0x6f, 0xfa, 0xb7, 0xd8, 0x67, 0xfe, 0xd6, 0x28, 0x10, 0x06, 0x0c, 0x04, 0xd6, 0x40, 0xfe, 0x40, 0x30, 0xf9, 0xc6, 0x40]
